b9cd3389ce1a45a1ecbbeefd8fea6de13680f507
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-07-06 14:53:22 +0300

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,15 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
-    //     }
-    // }
-
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    println!("Cool Stuff{}", "!?!");
 
     loop {}
 }
 
 // This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
